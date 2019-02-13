d6f3df7c5fcf351d1b45319e542f67f2df5ecad8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-02-13 18:36:27 +0530

    RMX1921: Add dirac blobs

--- RMX1921-vendor.mk
+++ RMX1921-vendor.mk
@@ -1435,6 +1435,12 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1921/proprietary/vendor/radio/qcril_database/upgrade/4_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/4_version_update_ecc_table.sql \
     vendor/realme/RMX1921/proprietary/vendor/radio/qcril_database/upgrade/5_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/5_version_update_ecc_table.sql
 
+#Dirac
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX1921/proprietary/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    vendor/realme/RMX1921/proprietary/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX1921/proprietary/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so
+
 PRODUCT_PACKAGES += \
     libantradio \
     libplatformconfig \
